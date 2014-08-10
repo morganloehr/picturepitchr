273d4d27b6b7f3796d70e4e6d8a679cf5b5f0a14
Morgan Loehr <morganloehr@gmail.com>
2014-08-10 09:56:11 -0700

    first commit

---
+++ vendor/bundle/gems/yajl-ruby-1.1.0/ext/yajl/siteconf20140802-23543-9b5vdv.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140802-23543-4c3chx"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
