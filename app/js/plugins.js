471a3e6893462eede0236fa2da911c5ced034e6d
Morgan Loehr <morganloehr@gmail.com>
2014-08-12 19:59:22 -0700

    fixed header plug in

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -107,7 +107,7 @@ if ( typeof define === 'function' && define.amd ) {
 
 
 /**
- * cbpAnimatedHeader.min.js v1.0.0
+ * cbpAnimatedHeader.js v1.0.0
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
@@ -116,4 +116,37 @@ if ( typeof define === 'function' && define.amd ) {
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var cbpAnimatedHeader=(function(){var b=document.documentElement,g=document.querySelector(".cbp-af-header"),e=false,a=300;function f(){window.addEventListener("scroll",function(h){if(!e){e=true;setTimeout(d,250)}},false)}function d(){var h=c();if(h>=a){classie.add(g,"cbp-af-header-shrink")}else{classie.remove(g,"cbp-af-header-shrink")}e=false}function c(){return window.pageYOffset||b.scrollTop}f()})();
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.cbp-af-header' ),
+        didScroll = false,
+        changeHeaderOn = 300;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'cbp-af-header-shrink' );
+        }
+        else {
+            classie.remove( header, 'cbp-af-header-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
